dec668d6c2869a3a07c19b747d098c022c07ca81
Henrik86 <henrik86@henrik86s-mbp.dsldevice.lan>
2017-11-12 21:41:04 +0700

    Using localStorate; CSS Pre-processor by SASS, etc.

--- AI&T/senior-webapp-testing/README.md
+++ AI&T/senior-webapp-testing/README.md
@@ -1,14 +1,15 @@
 # Feedback from Applicant
 - In data.js, at record id 10, item name isn't normal, it has special characters so I removed them at reformating data step.
+- As following, I have checked on requirements, which were implemented or considered.
 
 # Prerequisites
 
 1. [x]  Pure js implementation without any libraries or frameworks is strongly recommended.
 2. [x]  It should be bug‐free on latest Chrome(PC), which also means you can use latest js spec if supported.
-3. [ ]  CSS preprocessor is strongly recommended.
+3. [x]  CSS preprocessor is strongly recommended.
 4. [x]  Must use git for your commit.
-5. [ ]  Clear annotations are strongly recommended.
-6. [ ]  Pack your final work as .zip file, which should contain: 
+5. [x]  Clear annotations are strongly recommended.
+6. [x]  Pack your final work as .zip file, which should contain: 
 - All source code
 - A demo html page, which should work without extra setting
 - .git meta file, for us to see your commit log
@@ -31,5 +32,7 @@ There are many apps in Apple Sotre, please implement a suggest plugin for easier
 # Bonus point
 1. [x]  Update suggest layer with minimum dom operations rather than simply replacing the whole dom.
 2. [x]  Handle click event by event delegation, .etc
-3. [ ]  Make it extensible for other data source and template supports mobile browsers, such as iOS Safari. 
-4. [ ]  Should save input history when suggested item is selected. Manage input history in local browser(localStorage, indexDB, .etc) display history items with higher priority should enable user to clear special search history item.
+3. [x]  Make it extensible for other data source and template supports mobile browsers, such as iOS Safari. 
+- Supported JSON, JSONP, GET, POST data sources via AJAX Requests.
+- Supported mobile browsers by using Responsive Web Design.
+4. [x]  Should save input history when suggested item is selected. Manage input history in local browser(localStorage, indexDB, .etc) display history items with higher priority should enable user to clear special search history item.
--- AI&T/senior-webapp-testing/js/exam.js
+++ AI&T/senior-webapp-testing/js/exam.js
@@ -1,25 +1,88 @@
-var obj01 = new autoComplete({
-    selector: '#autocomplete01',
-    minChars: 0,
-    source: function (term, suggest) {
-        term = term.toLowerCase();
-        var choices = TABLE_DATA;
-        var suggestions = [];
-        for (i = 0; i < choices.length; i++) {
-            if (~(choices[i].name + ' ' + choices[i].id).toLowerCase().indexOf(term)) {
-                choices[i].name = choices[i].name.replace(/[^\w\s]/gi, '');
-                suggestions.push(choices[i]);
+(function () {
+    var names = null;
+
+    var obj01 = new autoComplete({
+        selector: '#autocomplete01',
+        minChars: 0,
+        source: function (term, suggest) {
+            term = term.toLowerCase();
+            var choices = TABLE_DATA;
+            var suggestions = [];
+            for (i = 0; i < choices.length; i++) {
+                if (~(choices[i].name + ' ' + choices[i].id).toLowerCase().indexOf(term)) {
+                    choices[i].name = choices[i].name.replace(/[^\w\s]/gi, '');
+                    suggestions.push(choices[i]);
+                }
+            }
+            suggest(suggestions);
+
+            loadInputHistory();
+        },
+        renderItem: function (item, search) {
+            search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&amp;');
+            var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
+            return '<div class="autocomplete-suggestion" data-langname="' + item.name + '" data-lang="' + item.id + '" data-val="' + search + '"><img class="item-image" src="' + item.thumbnailUrl + '"> ' + item.name.replace(re, "<b>$1</b>") + '</div>';
+        },
+        onSelect: function (e, term, item) {
+            console.log('Item "' + item.getAttribute('data-langname') + ' (' + item.getAttribute('data-lang') + ')" selected by ' + (e.type == 'keydown' ? 'pressing enter' : 'mouse click') + '.');
+            document.getElementById('autocomplete01').value = item.getAttribute('data-langname'); // + ' (' + item.getAttribute('data-lang') + ')';
+
+            saveInputHistory(document.getElementById('autocomplete01').value);
+        }
+    });
+
+    function loadInputHistory() {
+        if (names == null) {
+            if (typeof (Storage) !== "undefined") {
+                if (localStorage.getItem("names")) {
+                    names = JSON.parse(localStorage.getItem("names"));
+                } else {
+                    names = [];
+                }
+
+                if (names.length > 0) {
+                    names.forEach(element => {
+                        addInputHistoryUL(element);
+                    });
+                }
+
+                console.log("load: ");
+                console.info(names);
+            } else {
+                names = [];
             }
         }
-        suggest(suggestions);
-    },
-    renderItem: function (item, search) {
-        search = search.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&amp;');
-        var re = new RegExp("(" + search.split(' ').join('|') + ")", "gi");
-        return '<div class="autocomplete-suggestion" data-langname="' + item.name + '" data-lang="' + item.id + '" data-val="' + search + '"><img class="item_image" src="' + item.thumbnailUrl + '"> ' + item.name.replace(re, "<b>$1</b>") + '</div>';
-    },
-    onSelect: function (e, term, item) {
-        console.log('Item "' + item.getAttribute('data-langname') + ' (' + item.getAttribute('data-lang') + ')" selected by ' + (e.type == 'keydown' ? 'pressing enter' : 'mouse click') + '.');
-        document.getElementById('autocomplete01').value = item.getAttribute('data-langname'); // + ' (' + item.getAttribute('data-lang') + ')';
     }
-});
+
+    function saveInputHistory(newName) {
+        names.push(newName);
+
+        if (typeof (Storage) !== "undefined") {
+            localStorage.setItem("names", JSON.stringify(names));
+
+            addInputHistoryUL(newName);
+
+            console.log("save: ");
+            console.info(names);
+        }
+    }
+
+    function addInputHistoryUL(liValue) {
+        var ul = document.getElementById("inputhistory");
+        var li = document.createElement("li");
+        li.appendChild(document.createTextNode(liValue));
+        ul.appendChild(li);
+    }
+
+    document.getElementById("cleanInputHistory").onclick = function () {
+        names = [];
+        localStorage.setItem("names", JSON.stringify(names));
+
+        var ul = document.getElementById("inputhistory");
+        if (ul) {
+            while (ul.firstChild) {
+                ul.removeChild(ul.firstChild);
+            }
+        }
+    }
+}());
